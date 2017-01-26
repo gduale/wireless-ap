84676dc570858a61c919fc96a3896a139ed8c01c
Guillaume <guillaume.duale@gmail.com>
2017-01-26 16:31:23 +0100

    Access Point OK on Debian

--- wirelessAP.sh
+++ wirelessAP.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
+#Tested on Debian 8
 
 #Check if run as root
+if [ `whoami` == "root" ];then
+  echo "root user OK"
+else
+  echo "Please run it as root"
+fi
 
+#Stop the network manager to manage network card in command line
+service network-manager stop
 
 #VARS
-WAN=wlan0 #Interface already working with Internet active.
+WAN=wlan0 #Interface already working with Internet active. (/etc/network/interfaces)
 APCARD=wlan1
-
-#Maybe bypass the Network-manager
-#sudo service network-manager stop
-
 
 ##IPTABLES
 #ACCEPT
@@ -24,24 +28,30 @@ iptables -P OUTPUT ACCEPT
 
 #FLUSH all & clean the rest
 iptables -F
-#iptables -t nat -F
+iptables -t nat -F
 iptables -X
-#iptables -t nat -X
+iptables -t nat -X
 
-#Disable routing
-echo 0 > /proc/sys/net/ipv4/ip_forward
+#Enable routing
+echo 1 > /proc/sys/net/ipv4/ip_forward
 
 #Make an Ad-Hoc AP on the APCARD
-iwconfig $APCARD mode ad-hoc
+iwconfig $APCARD mode Ad-Hoc
 iwconfig $APCARD channel 11
 iwconfig $APCARD essid 'myAP'
-iwconfig $APCARD key 123456789
+iwconfig $APCARD key off
 
+#Setup the ip
+IP=192.168.8.55
+ifconfig $APCARD $IP/24
 
 #Route the traffic from the AP card to the Internet's card.
-#iptables -t nat -A POSTROUTING -o $WAN -j MASQUERADE
+iptables -t nat -A POSTROUTING -o $WAN -j MASQUERADE
 
-#iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-#iptables -A FORWARD -i eth0 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT
-#iptables -A FORWARD -i wlan0 -o eth0 -j ACCEPT
+#Send http traffic to Burp (Option for Burp : Proxy->Options->Proxy Listener->Request handling->Support invisible proxying (enable ony if needed)
+iptables -t nat -A PREROUTING -i $APCARD -p tcp --dport 80 -j REDIRECT --to-port 8080
+iptables -t nat -A PREROUTING -i $APCARD -p tcp --dport 443 -j REDIRECT --to-port 8080
+
+#dnsmasq / dhcp
+dnsmasq -d -q -i $APCARD -F 192.168.8.5,192.168.8.9,4h
 
